34026b484084cda58ae4a3dad630aae1899820ff
Denis Gorobets <d.gorobets@drweb.com>
2020-09-16 00:31:51 +0300

    struct as func arg

--- ch4/4.4_structs/4.4.1_structLiterals/examples/main.go
+++ ch4/4.4_structs/4.4.1_structLiterals/examples/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Point struct{ X, Y int }
 
 func main() {
 	p := Point{1, 2}
 	fmt.Println(p)
+
+	fmt.Println(Scale(p, 5))
+}
+
+func Scale(p Point, factor int) Point {
+	return Point{p.X * factor, p.Y * factor}
 }
