fff054d273f3f45c0b91ba67a9e6f2072969b8cb
Denis Gorobets <d.gorobets@drweb.com>
2021-01-26 23:44:24 +0300

    search GitHub issues

---
+++ ch4/4.5_JSON/issues/main.go
@@ -0,0 +1,20 @@
+// Issues prints a table of GitHub issues mathing the search terms
+package main
+
+import (
+	"fmt"
+	"gobook/ch4/4.5_JSON/github"
+	"log"
+	"os"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
